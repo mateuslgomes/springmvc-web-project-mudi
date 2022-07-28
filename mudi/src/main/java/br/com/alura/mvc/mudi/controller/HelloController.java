d7330b3b3fc78db68300f597038ab040b62a9925
Mateus Lopes Gomes <97681752+mateuslgomes@users.noreply.github.com>
2022-07-27 22:11:52 -0300

    conexão banco mysql/teste controller

---
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(HttpServletRequest request) {
+        request.setAttribute("nome", "Mateus");
+        return "hello";
+    }
+}
